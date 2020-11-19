c81824254a904962c61101de5949dd679f172b9e
daniloscosta <danilosanpaio89@gmail.com>
2020-11-19 16:53:25 -0300

    Commit aula 19-11-2020

---
+++ aula19112020/proprertiesString.js
@@ -0,0 +1,15 @@
+// Propriedade e métodos da String
+
+
+// (Length) Quantidade de caracteres
+console.log('Isso é uma string'.length);
+
+// Transforma letras maiúsculas em minúsculas
+console.log("TEXTO MAIÚSCULO PARA MINÚSCULO".toLowerCase());
+
+// Transforma letras minúsculas em maiúsculas
+console.log("texto maiúsculo para minúsculo".toUpperCase());
+
+// Elimina espaços na inserção de texto
+console.log(" Preparando     texto planilha");
+console.log("   Preparando texto planilha     ".trim());
--- aula19112020/typeString.js
+++ aula19112020/typeString.js
@@ -1,1 +1,34 @@
-//String em JS
+//Tipos de dados utilizados pelo JS
+
+//Strings em JS
+console.log('Isso é uma string');
+console.log("Isso é outra string");
+console.log(`Isso é mais uma string`);
+
+console.log("It's six o'clock");
+console.log(`Observe nessa "String"`);
+
+// Como imprimir caracteres especiais 
+/* 
+'
+" 
+\
+ quebra de linha 
+ retorno de carro 
+ tabulação vertical 
+ tabulação
+ backspace 
+*/
+
+console.log('\'');
+console.log("\"");
+console.log("\\");
+console.log("Como imprimir \ncaracteres especiais") // \n
+console.log("Como imprimir \rcaracteres especiais") // \r
+console.log("Como imprimir \vcaracteres especiais") // \v
+console.log("Como imprimir \tcaracteres especiais") // \t
+console.log("Como imprimir \bcaracteres especiais") // \b
+
+console.log("Isso é uma \'String\'");
+console.log("Outra \"String\"");
+
