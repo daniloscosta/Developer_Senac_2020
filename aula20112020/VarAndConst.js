a68b402f6417cbf765613b7a1df8a67396d7df12
daniloscosta <danilosanpaio89@gmail.com>
2020-11-20 16:52:03 -0300

    Commit aula20112020

---
+++ aula20112020/VarAndConst.js
@@ -0,0 +1,12 @@
+// Constantes
+const constante = 10;
+console.log(constante);
+
+// Variáveis
+let valor = 11;
+console.log(valor);
+
+valor = 8;
+console.log(valor);
+
+
---
+++ aula20112020/operatorsArit2.js
@@ -0,0 +1,20 @@
+// Operadores 
+// incremento (++)
+// decremento (--)
+// negação (-)
+// exponenciação (**)
+
+let num1 = 2;
+let num2;
+
+// incremento (++)
+console.log(num2 = ++num1);
+// decremento (--)
+console.log(num2 = --num1);
+// negação
+console.log(-num1);
+
+console.log(num2 = ++num1 ** num2);
+
+
+
