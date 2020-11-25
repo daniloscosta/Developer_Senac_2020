896de58c9a6350657cc1d339368673c017d9be8e
daniloscosta <danilosanpaio89@gmail.com>
2020-11-25 17:30:13 -0300

    aula23112020

---
+++ aula23112020/operadores.js
@@ -0,0 +1,19 @@
+// Operadors de atribuição
+
+let num1 = 5;
+let num2 = 11;
+
+// += 
+
+console.log('Resultado de soma: num1(5) += num2(11)', num1 += num2);
+console.log('Resultado da subtração: num1(5) -= num2(11)', num1 -= num2);
+console.log('Resultado de multiplicação: num1(5) *= num2(11)', num1 *= num2);
+console.log('Resultado de divisão: num1(5) /= num2(11)', num1 /= num2);
+console.log('Resultado de módulo: num1(5) %= num2(11)', num1 %= num2);
+console.log('Resultado de expeneciação: num1(5) **= num2(11)', num1 **= num2);
+console.log('Alô Mundo !');
+alert('Alô Mundo');
+
+let a = 5;
+let b = 20;
+console.log('O resultado de c é', a += b);
